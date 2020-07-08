9eec0fc121b2acc27739a84120cb43fcc6625b7e
luis Martínez <lucas.2451@hotmail.com>
2020-07-08 13:16:50 -0500

    Success

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 # Resolve the problem!!
-
+import re
 
 def run():
     # Start coding here
+    msg = ''
+
+    with open('./encoded.txt',mode='r',encoding='utf-8') as f:
+        msg=''.join(re.findall('[a-z]',f.read()))
+
+    print('El mensaje oculto es: ',msg)
 
 
 if __name__ == '__main__':
